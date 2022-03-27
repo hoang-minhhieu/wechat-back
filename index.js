438455d03eb1725349e5a3e582d17a84aa520b04
Minh Hieu HOANG <hoang.minhhieu7693@gmail.com>
2022-03-28 00:56:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,16 +14,31 @@ const io = new Server(server, {
   },
 });
 
+var usersList = [];
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
-  socket.on("join_room", (data) => {
-    socket.join(data);
+  //Event when an user joined a room
+  socket.on("join_room", (data, username) => {
     socket.room = data
+    socket.join(socket.room);
     socket.emit("user_socket_id", socket.id)
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    console.log("Total users of room " + socket.room + ": " + io.sockets.adapter.rooms.get(socket.room).size)
+    console.log(`User ${username} with ID: ${socket.id} joined room: ${socket.room}`);
+
+    //Create new User
+    const newUser = {
+      username: username,
+      socketId: socket.id
+    }
     
-    //Update number of users connected to all users in that room
+    //Add new object to list users
+    usersList.push(newUser)
+
+    //Send signal update list users to all clients in that room
+    io.in(socket.room).emit("receive_list_users", usersList); 
+    //Show total users of the room on server side
+    console.log("Total users of room " + socket.room + ": " + io.sockets.adapter.rooms.get(socket.room).size)
+    //Send signal update number of users connected to all users in that room
     io.in(socket.room).emit("receive_total_users", io.sockets.adapter.rooms.get(socket.room).size)
   });
 
@@ -33,11 +48,19 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
+    for(let i = 0; i < usersList.length; i++){
+      if (usersList[i].socketId === socket.id)
+        usersList.splice(i, 1)
+    }
+
+    if(usersList.length > 0)
+      io.in(socket.room).emit("receive_list_users", usersList); 
+
     if (io.sockets.adapter.rooms.get(socket.room)){
       io.in(socket.room).emit("receive_total_users", io.sockets.adapter.rooms.get(socket.room).size)
       console.log("Total users of room " + socket.room + ": " + io.sockets.adapter.rooms.get(socket.room).size)
     }
-    else console.log("Server empty. Waiting for user...")
+    else usersList = []
   });
 });
 
